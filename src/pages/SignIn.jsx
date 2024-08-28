ba0835b7f3ce27f4af8121f9d6c6f52bd29debb3
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2024-08-28 19:18:20 +0530

    style: login page

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import React from "react";
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
 
 const SignIn = () => {
   return (
-    <div>SignIn</div>
-  )
-}
+    <section className="w-full h-screen flex items-center justify-center">
+      <Card className="w-2/5">
+        <CardHeader>
+          <CardTitle>Welcome Back to ResuCraft!</CardTitle>
+          <CardDescription>
+          Log in to continue building your perfect resume.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          {/* <form>
+            <div className="grid w-full items-center gap-4">
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="name">Name</Label>
+                <Input id="name" placeholder="Name of your project" />
+              </div>
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="framework">Framework</Label>
+                <Input id="name" placeholder="Name of your project" />
+                
+              </div>
+            </div>
+          </form> */}
+        </CardContent>
+        <CardFooter className="flex justify-between">
+          {/* <Button variant="outline">Cancel</Button>
+          <Button>Deploy</Button> */}
+          <Button className={"w-10/12 mx-auto"}>Sign With Google</Button>
+        </CardFooter>
+      </Card>
+    </section>
+  );
+};
 
-export default SignIn
+export default SignIn;
