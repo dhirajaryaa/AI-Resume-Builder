8f4d3797190da9dcdf91614aef7038fd449e419d
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2024-08-28 13:43:29 +0530

    feat: add router like home,dashboard,signin

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Outlet } from 'react-router-dom';
 import "./App.css";
 
 const App = () => {
   return (
-    <>
-      
-    </>
+    <div className='grid-background'>
+      <Outlet />
+    </div>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,32 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App.jsx";
+import "./index.css";
+import {Home,Dashboard,SignIn}  from "./pages";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    element: <App />,
+    children:[
+      {
+      path:"/",
+      element:<Home />
+    },
+      {
+      path:"/dashboard",
+      element:<Dashboard />
+    },
+  ]
+  },
+  {
+    path: "/auth/signin",
+    element: <SignIn />,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
