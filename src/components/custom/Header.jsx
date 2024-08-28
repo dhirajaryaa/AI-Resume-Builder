fff344a5a0e59876a58be93d7ca09b2dc8316cf3
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2024-08-28 18:36:59 +0530

    add feature

--- src/components/custom/Header.jsx
+++ src/components/custom/Header.jsx
@@ -1,18 +1,20 @@
 import React from "react";
-import { Button } from "./ui/button";
+import { Button } from "../ui/button";
 import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <nav className="w-full shadow-lg h-16 flex items-center justify-between px-4 py-2 bg-primary ">
+    <nav className="w-full shadow-lg h-16 flex items-center justify-between px-4 py-2">
       {/* logo */}
       <Link to={"/"} className="w-6 sm:w-9 gap-2 flex items-center ">
         <img src="./logo.svg" alt="Logo" />
-        <span className="text-xl  sm:text-2xl  font-semibold text-white">ResuCraft</span>
+        <span className="text-xl  sm:text-2xl  font-semibold text-primary">
+          ResuCraft
+        </span>
       </Link>
       {/* signin  */}
       <Link to={"/auth/signin"}>
-        <Button variant="outline">SignIn</Button>
+        <Button >SignIn</Button>
       </Link>
     </nav>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,34 +4,96 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
-import { FAQ } from "@/data/faq";
+import { Button } from "@/components/ui/button";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+} from "@/components/ui/carousel";
+import FAQ from "../data/faq.json";
+import featured from "../data/featuredlist.json";
 import React from "react";
+import { Link } from "react-router-dom";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardTitle,
+} from "@/components/ui/card";
 
 const Home = () => {
   return (
-    <section>
+    <main className="flex flex-col gap-10 sm:gap:20 py-10 sm:py-20">
       {/* hero */}
-      <h1 className="text-xl font-bold">
-        Create Your Perfect Resume in Minutes with AI!
-      </h1>
-
-      {/* feature */}
+      <section className="text-center h-[60vh] flex items-center justify-center flex-col">
+        <h1 className="text-4xl sm:text-6xl lg:text-8xl font-extrabold">
+          Create Your Perfect <span className="text-primary">Resume</span>
+          <span className="block">
+            in Minutes with <span className="text-destructive">AI</span>
+          </span>
+        </h1>
+        <h3 className="text-xs sm:text-xl text-gray-700 font-semibold py-5 ">
+          Build a professional, standout resume that impresses recruiters with
+          our AI-powered resume builder
+        </h3>
+        <div className="py-8 sm:py-12">
+          {/* button  */}
+          <Link to={"/auth/signin"}>
+            <Button size="lg" variant={"destructive"} className={"text-lg"}>
+              Get Started
+            </Button>
+          </Link>
+        </div>
+      </section>
 
       {/* how to work */}
+      <section>
+        <h2 className="font-bold text-md sm:text-2xl text-center">
+          Why Choose Our AI Resume Builder?
+        </h2>
+        <Carousel className={"py-6"}>
+          <CarouselContent>
+            {featured.map((item, index) => {
+              return (
+                <CarouselItem key={index} className="basis-1/4 p-3">
+                  <div className="p-1">
+                    <Card>
+                      <CardContent className="flex flex-col bg-secondary aspect-square items-center justify-center w-full p-6">
+                        <div className="w-44 h-44 overflow-hidden">
+
+                        <img
+                          src={item.icon}
+                          alt={item.title}
+                          className="object-cover w-full h-full"
+                        />
+                        </div>
+                        <CardTitle className={"my-4 text-lg sm:text-xl"}>{item.title}</CardTitle>
+                        <CardDescription className="text-sm">{item.paragraph}</CardDescription>
+                      </CardContent>
+                    </Card>
+                  </div>
+                </CarouselItem>
+              );
+            })}
+           </CarouselContent>
+        </Carousel>
+      </section>
 
       {/* FAQ  */}
-
-      <Accordion type="single" collapsible>
-        {FAQ.map((item, index) => {
-          return (
-            <AccordionItem value={`item-${index}`} key={index}>
-              <AccordionTrigger>{item.question}</AccordionTrigger>
-              <AccordionContent>{item.answer}</AccordionContent>
-            </AccordionItem>
-          );
-        })}
-      </Accordion>
-    </section>
+      <section className="px-1 sm:px-[6rem]">
+        <h2 className="font-bold text-md sm:text-2xl">FAQ</h2>
+        <Accordion type="single" collapsible>
+          {FAQ.map((item, index) => {
+            return (
+              <AccordionItem value={`item-${index}`} key={index}>
+                <AccordionTrigger>{item.question}</AccordionTrigger>
+                <AccordionContent>{item.answer}</AccordionContent>
+              </AccordionItem>
+            );
+          })}
+        </Accordion>
+      </section>
+    </main>
   );
 };
 
