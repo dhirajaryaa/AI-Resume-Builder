6ce3227308d8f93cfcfc2978e6499fb0f636671a
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2024-08-29 22:53:19 +0530

    style: add dialog for creating new resume

--- src/components/custom/AddNewResume.jsx
+++ src/components/custom/AddNewResume.jsx
@@ -12,10 +12,10 @@ import { Input } from "../ui/input";
 
 const AddNewResume = () => {
   const [isOpenDialog, setIsOpenDialog] = useState(false);
-  const [input,setInput] = useState("");
+  const [input, setInput] = useState("");
 
   return (
-    <div>
+    <>
       <div
         onClick={() => setIsOpenDialog(true)}
         className="flex items-center justify-center h-[300px] border-2 rounded-xl hover:shadow-lg transition-all duration-200 bg-secondary border-dashed hover:scale-105 cursor-pointer"
@@ -29,18 +29,23 @@ const AddNewResume = () => {
             <DialogTitle>Create New Resume</DialogTitle>
             <DialogDescription>
               <p>Add a title for your new Resume</p>
-              <Input o className="my-2" placeholder={"Ex. Backend Developer"} />
+              <Input
+                onChange={(e) => setInput(e.target.value)}
+                className="my-2"
+                placeholder={"Ex. Backend Developer"}
+              />
             </DialogDescription>
             <div className="flex items-center gap-3 justify-end">
               <Button variant={"ghost"} onClick={() => setIsOpenDialog(false)}>
                 Cancel
               </Button>
-              <Button>Add</Button>
+
+              <Button disabled={!input.length > 0}>Add</Button>
             </div>
           </DialogHeader>
         </DialogContent>
       </Dialog>
-    </div>
+    </>
   );
 };
 
