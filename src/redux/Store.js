6d107c20bbcf260a15793fb1be63b02fc03a7807
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2024-08-31 16:35:30 +0530

    refactor: remove resuume edit slice

--- src/redux/Store.js
+++ src/redux/Store.js
@@ -1,12 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { AuthReducers } from "./auth/authSlice";
 import { DbReducer } from "./database/dbSlice";
-import { ResumeEditReducer } from "./resumeedit/resumeEditSlice";
 
 export const Store = configureStore({
   reducer: {
     auth: AuthReducers,
     db: DbReducer,
-    resumeEdit:ResumeEditReducer,
   },
 });
